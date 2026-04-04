8c6196408fd41d17495f6417fabc14aabe26df79
Sahu, Puspita <Puspita.Sahu@staples.com>
2026-04-04 02:14:14 -0400

    added system prompt

--- src/main/java/com/openai/service/OpenAIChatService.java
+++ src/main/java/com/openai/service/OpenAIChatService.java
@@ -1,5 +1,6 @@
 package com.openai.service;
 
+import com.openai.tools.ITSupportTicketTool;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 
@@ -8,15 +9,22 @@ public class OpenAIChatService{
 
     private final ChatClient chatClient;
     private final RealTimeDataTool realTimeDataTool;
+    private final ITSupportTicketTool itSupportTicketTool;
 
-    public OpenAIChatService(ChatClient chatClient,RealTimeDataTool realTimeDataTool){
+    public OpenAIChatService(ChatClient chatClient,RealTimeDataTool realTimeDataTool,ITSupportTicketTool itSupportTicketTool){
         this.chatClient = chatClient;
         this.realTimeDataTool=realTimeDataTool;
+        this.itSupportTicketTool = itSupportTicketTool;
     }
 
     public String askToAIStream(String message){
         return chatClient.prompt(message)
-                .tools(realTimeDataTool)
+                .system("""
+                        You are an IT support assistant.
+                        Help users create, update, and manage support tickets.
+                        Extract details like email, issue, and ticket id from user input
+                        """)
+                .tools(realTimeDataTool,itSupportTicketTool)
                 .call()
                 .content();
 
